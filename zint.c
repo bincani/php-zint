9ca685fa166428731eeffb6bb9fc2cd63936d341
Ben Incani <ben.incani@gmail.com>
2018-04-12 15:08:36 +1000

    php7 compat as per https://wiki.php.net/phpng-upgrading

--- zint.c
+++ zint.c
@@ -6,18 +6,19 @@
 #include <zint.h>
 
 #include "php.h"
+#include "ext/standard/info.h"
 #include "php_version.h"
- 
+
 #define PHP_ZINT_VERSION "0.5"
 #define PHP_ZINT_EXTNAME "zint"
-#define ZINT_VERSION "2.4.2"
- 
+#define ZINT_VERSION "2.4.1"
+
 extern zend_module_entry zint_module_entry;
 #define phpext_zint_ptr &zint_module_entry
- 
+
 // declaration of zint_barcode_file()
 PHP_FUNCTION(zint_barcode_file);
- 
+
 // list of custom PHP functions provided by this extension
 // set {NULL, NULL, NULL} as the last record to mark the end of list
 static zend_function_entry zint_functions[] = {
@@ -33,7 +34,7 @@ PHP_MINFO_FUNCTION(zint)
     php_info_print_table_row(2, "version of libzint", ZINT_VERSION);
     php_info_print_table_end();
 }
- 
+
 // the following code creates an entry for the module and registers it with Zend.
 zend_module_entry zint_module_entry = {
 #if ZEND_MODULE_API_NO >= 20010901
@@ -53,13 +54,13 @@ zend_module_entry zint_module_entry = {
 };
 
 ZEND_GET_MODULE(zint)
- 
+
 // implementation of zint_barcode_file()
 PHP_FUNCTION(zint_barcode_file)
 {
 	// structure to hold the barcode
 	struct zint_symbol *barcode;
-	
+
 	// holds error_code after barcode generation
 	int error_code = 0;
 
@@ -67,24 +68,26 @@ PHP_FUNCTION(zint_barcode_file)
 	zval **data;
 	HashTable *params_hash;
 	HashPosition pointer;
-	int params_count;
+	//int params_count;
 	char *key;
 	int key_len;
 	long index;
 
-	// receives the type parameter
-	long type;
-	
-	// resceives the input parameter
-	char *input;
-	int input_len;
 
-	// resceives the output_file parameter
+#if PHP_MAJOR_VERSION == 7
+    zend_long type;
+    zend_long rotate_angle;
+    size_t input_len;
+    size_t output_file_len;
+#else
+    long type;
+    long rotate_angle;
+    int input_len;
+    int output_file_len;
+#endif
+
+	char *input;
 	char *output_file;
-	int output_file_len;
-	
-	// receives the rotate_angle parameters
-	long rotate_angle;
 
 	// receives the optional parameters array
 	zval *optional_params;
@@ -94,35 +97,148 @@ PHP_FUNCTION(zint_barcode_file)
 	   &type, &input, &input_len, &output_file, &output_file_len, &rotate_angle, &optional_params) == FAILURE) {
 		RETURN_NULL();
 	}
-	
+
 	// only generate barcode with input data
 	if (input_len > 0) {
-		// initialize barcode structure		
+		// initialize barcode structure
 		barcode = ZBarcode_Create();
-		
+
 		// set barcode type
-		barcode->symbology = (type > 200) ? 0 : (int)type;
-		
+		barcode->symbology = ((int)type > 200) ? 1 : (int)type;
+
 		// set output file
 		strcpy(barcode->outfile, (output_file_len > 0) ? output_file : "barcode.png");
-		
+
 		// configure barcode from optional_params
-		params_hash = Z_ARRVAL_P(optional_params);
-		params_count = zend_hash_num_elements(params_hash);
-		for (zend_hash_internal_pointer_reset_ex(params_hash, &pointer); 
 #if PHP_MAJOR_VERSION == 7
-		   zend_hash_get_current_data(params_hash) == SUCCESS;
+
+    	zend_string *key;
+    	zval *val;
+
+		ZEND_HASH_FOREACH_STR_KEY_VAL(Z_ARRVAL_P(optional_params), key, val) {
+            if (strcmp(ZSTR_VAL(key), "height") == 0) {
+    			if (Z_TYPE_P(val) == IS_LONG && Z_LVAL_P(val) < 1000) {
+    				barcode->height = Z_LVAL_P(val);
+    			}
+    			else {
+    				strcpy(barcode->errtxt, "error: given value for height is invalid");
+    				error_code = 1;
+    			}
+			}
+			else if (strcmp(ZSTR_VAL(key), "whitespace_width") == 0) {
+				if (Z_TYPE_P(val) == IS_LONG && Z_LVAL_P(val) < 1000) {
+					barcode->whitespace_width = Z_LVAL_P(val);
+				}
+				else {
+					strcpy(barcode->errtxt, "error: given value for whitespace_width is invalid");
+					error_code = 1;
+				}
+			}
+			else if (strcmp(ZSTR_VAL(key), "border_width") == 0) {
+				if (Z_TYPE_P(val) == IS_LONG && Z_LVAL_P(val) < 1000) {
+					barcode->border_width = Z_LVAL_P(val);
+				}
+				else {
+					strcpy(barcode->errtxt, "error: given value for border_width is invalid");
+					error_code = 1;
+				}
+			}
+			else if (strcmp(ZSTR_VAL(key), "output_options") == 0) {
+				if (Z_TYPE_P(val) == IS_STRING && (strcmp(Z_STRVAL_P(val), "bind") == 0 || strcmp(Z_STRVAL_P(val), "box") == 0)) {
+					if (strcmp(Z_STRVAL_P(val), "bind") == 0) {
+						barcode->output_options = BARCODE_BIND;
+					}
+					else {
+						barcode->output_options = BARCODE_BOX;
+					}
+				}
+				else {
+					strcpy(barcode->errtxt, "error: given value for output_options is invalid");
+					error_code = 1;
+				}
+			}
+			else if (strcmp(ZSTR_VAL(key), "fgcolour") == 0) {
+				if (Z_TYPE_P(val) == IS_STRING && Z_STRLEN_P(val) == 6) {
+					strcpy(barcode->fgcolour, Z_STRVAL_P(val));
+				}
+				else {
+					strcpy(barcode->errtxt, "error: given value for fgcolour is invalid");
+					error_code = 1;
+				}
+			}
+			else if (strcmp(ZSTR_VAL(key), "bgcolour") == 0) {
+			    if (Z_TYPE_P(val) == IS_STRING && Z_STRLEN_P(val) == 6) {
+			        strcpy(barcode->bgcolour, Z_STRVAL_P(val));
+				}
+				else {
+					strcpy(barcode->errtxt, "error: given value for bgcolour is invalid");
+					error_code = 1;
+				}
+			}
+			else if (strcmp(ZSTR_VAL(key), "scale") == 0) {
+				if (Z_TYPE_P(val) == IS_DOUBLE) {
+					barcode->scale = Z_DVAL_P(val);
+				}
+				else {
+					strcpy(barcode->errtxt, "error: given value for scale is invalid");
+					error_code = 1;
+				}
+			}
+			else if (strcmp(ZSTR_VAL(key), "input_mode") == 0) {
+				if (
+				    Z_TYPE_P(val) == IS_STRING
+				    &&
+				    (
+				        strcmp(Z_STRVAL_P(val), "data") == 0
+				        ||
+				        strcmp(Z_STRVAL_P(val), "unicode") == 0
+				        ||
+				        strcmp(Z_STRVAL_P(val), "gs1") == 0)
+                ) {
+					if (strcmp(Z_STRVAL_P(val), "unicode") == 0) {
+						barcode->input_mode = UNICODE_MODE;
+					}
+					else if (strcmp(Z_STRVAL_P(val), "gs1") == 0) {
+						barcode->input_mode = GS1_MODE;
+					}
+					else {
+						barcode->input_mode = DATA_MODE;
+					}
+				}
+				else {
+					strcpy(barcode->errtxt, "error: given value for input_mode is invalid");
+					error_code = 1;
+				}
+			}
+			else if (strcmp(ZSTR_VAL(key), "primary") == 0) {
+				if (Z_TYPE_P(val) == IS_STRING && Z_STRLEN_P(val) > 0) {
+					strcpy(barcode->primary, Z_STRVAL_P(val));
+				}
+				else {
+					strcpy(barcode->errtxt, "error: given value for primary is invalid");
+					error_code = 1;
+				}
+			}
+			else if (strcmp(ZSTR_VAL(key), "notext") == 0) {
+				if (Z_TYPE_P(val) == IS_LONG && (Z_LVAL_P(val) == 0 || Z_LVAL_P(val) == 1)) {
+					// show human readable text
+					barcode->show_hrt = (Z_LVAL_P(val) == 1 ? 0 : 1);
+				}
+				else {
+					strcpy(barcode->errtxt, "error: given value for notext is invalid");
+					error_code = 1;
+				}
+			}
+		} ZEND_HASH_FOREACH_END();
 #else
-	           zend_hash_get_current_data_ex(params_hash, (void **)&data, &pointer) == SUCCESS;
-#endif
-		   zend_hash_move_forward_ex(params_hash, &pointer)) {
+        params_hash = Z_ARRVAL_P(optional_params);
+		for (
+		    zend_hash_internal_pointer_reset_ex(params_hash, &pointer);
+            zend_hash_get_current_data_ex(params_hash, (void **)&data, &pointer) == SUCCESS;
+            zend_hash_move_forward_ex(params_hash, &pointer)) {
 			zval temp = **data;
 			zval_copy_ctor(&temp);
-#if PHP_MAJOR_VERSION == 7
-			if (zend_hash_get_current_key(params_hash, &key, &key_len) == HASH_KEY_IS_STRING) {
-#else
-			if (zend_hash_get_current_key_ex(params_hash, &key, &key_len, &index, 0, &pointer) == HASH_KEY_IS_STRING) {
-#endif
+            if (zend_hash_get_current_key_ex(params_hash, &key, &key_len, &index, 0, &pointer) == HASH_KEY_IS_STRING) {
 				if (strcmp(key, "height") == 0) {
 					if (Z_TYPE(temp) == IS_LONG && Z_LVAL(temp) < 1000) {
 						barcode->height = Z_LVAL(temp);
@@ -206,34 +322,32 @@ PHP_FUNCTION(zint_barcode_file)
 					}
 				}
 			}
-
 			zval_dtor(&temp);
 		}
-		
+#endif
 		// encode barcode and save file
-		if(error_code == 0) {
+		if (error_code == 0) {
 			error_code = ZBarcode_Encode_and_Print(barcode, input, input_len, (rotate_angle > 270) ? 0 : (int)rotate_angle);
 		}
-
 		if(error_code == 0) {
 			// return true on success
 			RETVAL_BOOL(1);
 		} else {
 			// return error_code on error
-#if PHP_MAJOR_VERSION == 7			
-			RETVAL_STRINGL(barcode->errtxt, 1);
+#if PHP_MAJOR_VERSION == 7
+            RETVAL_STRINGL(barcode->errtxt, 1);
 #else
-			RETVAL_STRING(barcode->errtxt, 1);
+            RETVAL_STRING(barcode->errtxt, 1);
 #endif
 		}
-
 		// cleanup structure
 		ZBarcode_Delete(barcode);
-	} else {
+	}
+	else {
 #if PHP_MAJOR_VERSION == 7
-		RETVAL_STRINGL("error: no input data given", 1);
+        RETVAL_STRINGL("error: no input data given", 1);
 #else
-		RETVAL_STRING("error: no input data given", 1);
+        RETVAL_STRING("error: no input data given", 1);
 #endif
 	}
 }
